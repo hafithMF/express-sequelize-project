5de3b866c1736ba0639cc83a33b1e768311b0d5a
mfarhannr <muhammad755262@smk.belajar.id>
2024-05-16 15:42:28 +0700

    express-sequelize-project lanjut

---
+++ migrations/20240516075628-create-table-user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {    
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      emial: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        }
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        onUpdate: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+     })
+  },
+
+  async down (queryInterface, Sequelize) {
+  await queryInterface.dropTable('users');
+  }
+};
